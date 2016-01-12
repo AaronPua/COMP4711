993b752e619955e72826524400b167ac739fd6ec
Silverwood <aaronpua@hotmail.com>
2016-01-12 13:53:36 -0800

    Strings, Operators, Expressions, Selection

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php     
+        $name = 'Jim';
+        $what = 'geek';
+        $level = 10;
+        echo 'Hi, my name is '.$name,'. and I am a level '.$level.''.$what;
+        
+        echo '<br/>';
+        
+        $hoursworked = 10;
+        $rate = 12;
+        $total = $hoursworked * $rate;
+        echo 'You owe me '.$total;
+        
+        if($hoursworked > 40) {
+            $total = $hoursworked * $rate * 1.5;
+        } else {
+            $total = $hoursworked * $rate;
+        }
+        echo ($total > 0) ? 'You owe me '.$total : "You're welcome";
+        ?>
+    </body>
