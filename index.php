6bd8a76a55a6c6ec3fc248e1436e60778894b564
Silverwood <aaronpua@hotmail.com>
2016-01-15 09:43:13 -0800

    Functions v2

--- index.php
+++ index.php
@@ -10,21 +10,34 @@
         $squares = str_split($position);
         
         function winner($token, $position) {
-            $won = false;
-            if(($position[0] == $token) &&
-               ($position[1] == $token) &&
-               ($position[2] == $token)) { 
-                $won = true;
-            } else if (($position[3] == $token) &&
-                        ($position[4] == $token) &&
-                        ($position[5] == $token)) {
-                //...
-            }
-            return $won;
-        }
-         if(winner('x', $squares)) echo 'You Win.';
-         else if (winner('o', $squares)) echo 'I win.';
-         else echo 'No Winner Yet.';
+ +            $won = false;
+ +            if (($position[0] == $token) && ($position[1] == $token) && ($position[2] == $token)) {
+ +                $won = true;
+ +            }
+ +            if (($position[3] == $token) && ($position[4] == $token) && ($position[5] == $token)) {
+ +                $won = true;
+ +            }
+ +            if (($position[7] == $token) && ($position[6] == $token) && ($position[8] == $token)) {
+ +                $won = true;
+ +            }
+ +            if (($position[0] == $token) && ($position[3] == $token) && ($position[6] == $token)) {
+ +                $won = true;
+ +            }
+ +            if (($position[1] == $token) && ($position[4] == $token) && ($position[7] == $token)) {
+ +                $won = true;
+ +            }
+ +            if (($position[2] == $token) && ($position[5] == $token) && ($position[8] == $token)) {
+ +                $won = true;
+ +            }
+ +            if (($position[0] == $token) && ($position[4] == $token) && ($position[8] == $token)) {
+ +                $won = true;
+ +            }
+ +            if (($position[6] == $token) && ($position[4] == $token) && ($position[2] == $token)) {
+ +                $won = true;
+ +            }
+ +            
+ +            return $won;
+          }
         ?>        
     </body>
 </html>
