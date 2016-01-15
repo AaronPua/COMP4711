95f16d5db4f716e9dcfcf0b29236f76824bdea5d
Silverwood <aaronpua@hotmail.com>
2016-01-15 08:38:30 -0800

    Input

--- index.php
+++ index.php
@@ -13,10 +13,9 @@
         
         echo '<br/>';
         
-        $hoursworked = 10;
+        $hoursworked = $_GET['hours'];
         $rate = 12;
         $total = $hoursworked * $rate;
-        echo 'You owe me '.$total;
         
         if($hoursworked > 40) {
             $total = $hoursworked * $rate * 1.5;
